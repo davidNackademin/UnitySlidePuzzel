68e38b9bb5e059960b73834d6928d8885e280a2f
David Svensson <david@Admins-MacBook-Pro.local>
2018-08-21 10:22:27 +0200

    Tiles and TileManager created

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour {
+
+    public TileManager tileManager;
+    public bool empty = false;
+
+    private void OnMouseDown()
+    {
+        // Debug.Log("Clicked: " + gameObject.name);
+        tileManager.TilePressed(this);
+    }
+
+}
